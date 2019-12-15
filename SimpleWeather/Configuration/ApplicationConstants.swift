24199badfe521943934e00c2e3b2957ce4a6b840
Jakub Kurgan <jakubkurgan@gmail.com>
2019-12-15 20:09:21 +0100

    Added application constants, localizable stings, data and string extensions.

---
+++ SimpleWeather/Configuration/ApplicationConstants.swift
@@ -0,0 +1,15 @@
+//
+//  ApplicationConstants.swift
+//  SimpleWeather
+//
+//  Created by Jakub Kurgan on 15/12/2019.
+//  Copyright © 2019 Jakub Kurgan. All rights reserved.
+//
+
+import Foundation
+
+enum ApplicationConstants {
+    enum Main: String {
+        case applicationName = "SimpleWeather"
+    }
+}
---
+++ SimpleWeather/Extensions/Data+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  Data+Extension.swift
+//  SimpleWeather
+//
+//  Created by Jakub Kurgan on 15/12/2019.
+//  Copyright © 2019 Jakub Kurgan. All rights reserved.
+//
+
+import Foundation
+
+extension Data {
+    func decoded<T:Decodable>(using decoder: AnyDecoder = JSONDecoder()) throws -> T {
+        return try decoder.decode(T.self, from: self)
+    }
+}
+
+protocol AnyDecoder {
+    func decode<T: Decodable>(_ type: T.Type, from data: Data) throws -> T
+}
+
+extension JSONDecoder: AnyDecoder {}
